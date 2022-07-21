77cd1fc3b9ca0eecae2dd3d2ca4ecf489acae72a
Sarath T S <sarathiscookie@hotmail.com>
2022-07-21 14:58:24 +0530

    Router links added for global components (app.js).

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,9 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                <router-link class="navbar-brand" to="/">{{ config('app.name', 'Laravel') }}</router-link>
+                
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -51,6 +50,10 @@
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
+
+                            <li class="nav-item">
+                                <router-link class="nav-link" to="/contact">Contact US</router-link>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
