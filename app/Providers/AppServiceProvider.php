3245106ed18a833a19588ecd7bb89008cb80111c
Sarath T S <sarathiscookie@hotmail.com>
2022-07-27 21:46:04 +0530

    Database: Enable it. If you are running MySQL or MariaDB older versions.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Schema::defaultStringLength(191); // Enable it. If you are running a version of MySQL older than the 5.7.7 release or MariaDB older than the 10.2.2 release.
     }
 }
