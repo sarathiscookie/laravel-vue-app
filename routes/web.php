5f02b37c7ffc29c0f958f17b8ec0c57e17259048
Sarath T S <sarathiscookie@hotmail.com>
2022-08-22 21:47:19 +0530

    UI Bootsrap added.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/{any?}', function () {
 Auth::routes();
 
 #Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
